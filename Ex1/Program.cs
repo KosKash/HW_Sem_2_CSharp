cb61d499e51fd0fc49646c7b1e78758d0de22053
Constantine <koskash98ksband@gmail.com>
2022-06-28 17:48:21 +0600

    HW1_done

---
+++ Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int num = Prompt("Input number ");
+int a = num % 100;
+int b = a / 10;
+Console.Write("Second number is - " + b);
