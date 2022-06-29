7a241a31ddddff5023a04d62ba151bfde90cb1c3
Constantine <koskash98ksband@gmail.com>
2022-06-29 12:43:21 +0600

    Ex3 Done

---
+++ Ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа
+или сообщает, что третьей цифры нет.*/
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int num = Prompt("Введите число ");
+if (num > 100)
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    int result = num%10;
+        Console.WriteLine("Третья цифра от числа - " + result);
+}
+else
+{
+    Console.WriteLine("В числе - " + num + " третьей цифры нет");
+}
