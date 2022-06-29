169aed92368b5477c9adee52b25faf197b9f13fd
Constantine <koskash98ksband@gmail.com>
2022-06-29 11:28:24 +0600

    Ex2 done

---
+++ Ex2/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Напишите программу, которая выводит случайное 
+трёхзначное число и удаляет вторую цифру этого числа.*/
+Console.Clear();
+int a = new Random().Next(100, 999);
+int first = a / 100;
+int second = a % 10;
+int result = first*10+second;
+Console.WriteLine("Из числа " + a + " получается число " + result);
